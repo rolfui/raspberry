f30a94a99691d62f9ccbf8fbf2510f3e39ca3ada
Rolf Ulrich Isachsen <rolf@isachsen.org>
2020-04-05 23:59:28 +0200

    cotemp updated with automatic temp sensor

--- cotemp.py
+++ cotemp.py
@@ -16,28 +16,27 @@ ser = serial.Serial(
 
 base_dir = '/sys/bus/w1/devices/'
 
-device_file = [base_dir + '28-00000930655c' + '/w1_slave']
-device_file.append(base_dir + '28-00000930882a' + '/w1_slave')
+dirs = os.listdir(base_dir)
+#dirs.remove('w1_bus_master1')
+dirs.sort()
+dirs.pop()
 
-def read_temp_raw(dev):
-    f = open(device_file[dev], 'r')
-    lines = f.readlines()
-    while lines[0].strip()[-3:] != 'YES':
-        time.sleep(0.2)
-        lines = r.readlines()
-    f.close()
-    return lines
-
-def extract_temp(lines):
-    equals_pos = lines[1].find('t=')
-    if equals_pos != -1:
-        temp_string = lines[1][equals_pos+2:]
-        temp = float(temp_string) / 1000.0
-        return temp
+device_files = []
+for dir in dirs:
+    device_files.append(base_dir + dir + '/w1_slave')
 
 def read_temp():
-    lines = [read_temp_raw(0),read_temp_raw(1)]
-    temp = [extract_temp(lines[0]),extract_temp(lines[1])]
+    temp = []
+    for device_file in device_files:
+        f = open(device_file, 'r')
+        lines = f.readlines()
+        while lines[0].strip()[-3:] != 'YES':
+            time.sleep(0.2)
+            lines = r.readlines()
+        f.close()
+        equals_pos = lines[1].find('t=')
+        temp_string = lines[1][equals_pos+2:]
+        temp.append(float(temp_string)/1000.0)
     return temp
 
 def read_co2():
@@ -51,14 +50,19 @@ def read_co2():
 
 file_name = 'log-' + time.strftime('%H%M%S', time.localtime()) + '.txt'
 print('Filename:' , file_name)
-print('Format: HHMMSS, CO2 in ppm, Temperaturer i Celsius:')
 while True:
     temp = read_temp();
-    tid = time.strftime('%H%M%S', time.localtime())
+    tid = time.strftime('%Y%m%d%H%M%S', time.localtime())
+    #tid = int(time.time())
+    #print(tid)
     co2 = read_co2()
-    outString = tid + ',' + f'{co2}'.zfill(5) + ',' + f'{temp[0]:.3f}'.zfill(7) + ','  + f'{temp[1]:.3f}'.zfill(7)
+    #outString = str(tid) + ',' + f'{co2}'.zfill(5) + ',' + f'{temp[0]:.3f}'.zfill(7) + ','  + f'{temp[1]:.3f}'.zfill(7)
+    #outString = str(tid) + ',' + str(co2) + ',' + str(temp[0]) + ','  + str(temp[1])
+    outString = tid + ',' + str(co2)
+    for tmp in temp:
+        outString +=  ',' + str(tmp)
     print(outString)
     f = open(file_name,"a")
     f.write(outString + '\r\n')
     f.close()
-    #time.sleep(60)
+    time.sleep(60)
