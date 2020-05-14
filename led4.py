a1c19f9a2b1ae435ec2459603cb6129b3f0cdc31
Rolf Ulrich Isachsen <rolf@isachsen.org>
2020-05-14 10:14:37 +0200

    added led4.py

--- led4.py
+++ led4.py
@@ -1,1 +1,12 @@
-    https://www.raspberrypi.org/documentation/usage/gpio/python/README.md
+from gpiozero import LED
+
+RED = LED(25)
+BUTTON = Button(8)
+
+while True:
+    if BUTTON.is_pressed:
+        RED.on()
+    else:
+        RED.off()
+
+#https://www.raspberrypi.org/documentation/usage/gpio/python/README.md
